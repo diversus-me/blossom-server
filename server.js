94e38dc52edde836f474f272befe26d6eaf95282
PeterKey <PeterKey@users.noreply.github.com>
2019-04-22 14:09:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-import path from 'path';
-import express from 'express';
-const PORT = process.env.HTTP_PORT || 4001;
-const app = express();
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.get('/', (req, res) => {
-  res.send('just gonna send it');
-});
-app.get('/flower', (req, res) => {
+import path from 'path'
+import express from 'express'
+
+const PORT = process.env.HTTP_PORT || 5000
+const app = express()
+
+
+app.get('/flowers', (req, res) => {
   res.json({
     name: 'Dandelion',
     colour: 'Blue-ish'
-  });
-});
+  })
+})
+
+
+
 app.listen(PORT, () => {
-  console.log(`Server listening at port ${PORT}.`);
-});
+  console.log(`Server listening at port ${PORT}.`)
+})
