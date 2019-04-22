465569be2eff4d4d99a1fc6c4d39e329d775e9c0
PeterKey <PeterKey@users.noreply.github.com>
2019-04-22 15:46:09 +0200

    Updated Build Procedure

---
+++ build/server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var _path = _interopRequireDefault(require("path"));
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var PORT = process.env.HTTP_PORT || 5000;
+var app = (0, _express["default"])();
+app.get('/flowers', function (req, res) {
+  res.json({
+    name: 'Dandelion',
+    colour: 'Blue-ish'
+  });
+});
+app.listen(PORT, function () {
+  console.log("Server listening at port ".concat(PORT, "."));
+});
