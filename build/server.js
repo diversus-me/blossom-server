63ef71c16d0a4500d273c5aa2843d08bf4348c5f
PeterKey <PeterKey@users.noreply.github.com>
2019-04-23 00:52:50 +0200

    Update server.js

--- build/server.js
+++ build/server.js
@@ -6,7 +6,7 @@ var _express = _interopRequireDefault(require("express"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var PORT = process.env.HTTP_PORT || 5000;
+var PORT = process.env.HTTP_PORT || 8081;
 var app = (0, _express["default"])();
 app.get('/flowers', function (req, res) {
   res.json({
