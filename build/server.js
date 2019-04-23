d62917f415a38ab37e8b8911172f3113c5d2d233
PeterKey <PeterKey@users.noreply.github.com>
2019-04-23 10:29:13 +0200

    Updated Server with DB connection

--- build/server.js
+++ build/server.js
@@ -6,6 +6,21 @@ var _express = _interopRequireDefault(require("express"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+var pgp = require('pg-promise')();
+
+var connection = {};
+
+if (process.env.NODE_ENV === 'production') {
+  var _connection = {
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD
+  };
+}
+
+var db = pgp(connection);
 var PORT = process.env.HTTP_PORT || 8081;
 var app = (0, _express["default"])();
 app.get('/flowers', function (req, res) {
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 import path from 'path'
 import express from 'express'
 
+const pgp = require('pg-promise')()
+const connection = {}
+
+if (process.env.NODE_ENV === 'production') {
+  const connection = {
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD
+  }
+}
+
+const db = pgp(connection)
+
 const PORT = process.env.HTTP_PORT || 8081
 const app = express()
-
 
 app.get('/flowers', (req, res) => {
   res.json({
@@ -11,8 +25,6 @@ app.get('/flowers', (req, res) => {
     colour: 'Blue-ish'
   })
 })
-
-
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`)
