fd6cad0ca78ff6e2089a4c29331500e8772f15ad
PeterKey <PeterKey@users.noreply.github.com>
2019-04-24 21:31:57 +0200

    Update server.js

--- build/server.js
+++ build/server.js
@@ -2,9 +2,7 @@
 
 var _express = _interopRequireDefault(require("express"));
 
-var _sequelize = _interopRequireWildcard(require("sequelize"));
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
+var _sequelize = _interopRequireDefault(require("sequelize"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -20,18 +18,16 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 //   }
 // }
 // const db = pgp(connection)
-var db = new _sequelize.Sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+var db = new _sequelize["default"](process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
   host: process.env.RDS_HOSTNAME,
   port: process.env.RDS_PORT,
   dialect: 'postgres'
 });
-
-_sequelize["default"].authenticate().then(function () {
+db.authenticate().then(function () {
   console.log('Connection to database established.');
 })["catch"](function (err) {
   console.error('Unable to connect to the database:', err);
 });
-
 var PORT = process.env.HTTP_PORT || 8081;
 var app = (0, _express["default"])();
 app.get('/flowers', function (req, res) {
