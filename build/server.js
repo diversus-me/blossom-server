2726755a0228206aa869db92855007f8c7ecf69c
PeterKey <PeterKey@users.noreply.github.com>
2019-04-26 14:07:19 +0200

    Update server.js

--- build/server.js
+++ build/server.js
@@ -2,40 +2,33 @@
 
 var _express = _interopRequireDefault(require("express"));
 
-var _sequelize = _interopRequireDefault(require("sequelize"));
+var _connectPostgres = _interopRequireDefault(require("./postgres/connectPostgres"));
+
+var _initPostgres = _interopRequireDefault(require("./postgres/initPostgres"));
+
+var _initializeSessions = _interopRequireDefault(require("./sessions/initializeSessions"));
+
+var _defineAPI = _interopRequireDefault(require("./defineAPI"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// const pgp = require('pg-promise')()
-// const connection = {}
-// if (process.env.NODE_ENV === 'production') {
-//   const connection = {
-//     host: process.env.RDS_HOSTNAME,
-//     port: process.env.RDS_PORT,
-//     database: process.env.RDS_DB_NAME,
-//     user: process.env.RDS_USERNAME,
-//     password: process.env.RDS_PASSWORD
-//   }
-// }
-// const db = pgp(connection)
-var db = new _sequelize["default"](process.env.RDS_DB_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
-  host: process.env.RDS_HOSTNAME,
-  port: process.env.RDS_PORT,
-  dialect: 'postgres'
-});
-db.authenticate().then(function () {
+var PORT = process.env.HTTP_PORT || 8081;
+var app = (0, _express["default"])();
+/* Connect to Database */
+
+var postgres = (0, _connectPostgres["default"])();
+postgres.authenticate().then(function () {
   console.log('Connection to database established.');
 })["catch"](function (err) {
   console.error('Unable to connect to the database:', err);
 });
-var PORT = process.env.HTTP_PORT || 8081;
-var app = (0, _express["default"])();
-app.get('/flowers', function (req, res) {
-  res.json({
-    name: 'Dandelion',
-    colour: 'Blue-ish'
-  });
-});
+/* Sync with Database */
+
+var models = (0, _initPostgres["default"])(postgres);
+(0, _initializeSessions["default"])(app);
+/* Initiate API */
+
+(0, _defineAPI["default"])(app, models);
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
 });
