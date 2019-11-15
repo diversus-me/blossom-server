1e58c1f3150514e22a93d6bcc9ce0ede90340575
PeterKey <p.korcsak@googlemail.com>
2019-11-15 03:31:44 +0100

    Changed Session Store to Redis

--- src/sessions/initializeSessions.js
+++ src/sessions/initializeSessions.js
@@ -1,49 +1,37 @@
 import session from 'express-session'
-import dynamoDB from 'connect-dynamodb'
-import postgres from 'connect-pg-simple'
-import pg from 'pg'
+import redis from 'redis'
+import redisStore from 'connect-redis'
 
 const maxAge = 2629746000
 
 export default function initializeSessions (app) {
   let store = {}
   if (process.env.NODE_ENV === 'production') {
-    const dynamoOptions = {
-      table: 'blossom-sessions',
-      hashKey: 'sessionID',
-      AWSConfigJSON: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: 'eu-central-1'
-      },
-      readCapacityUnits: 5,
-      writeCapacityUnits: 5
-    }
-
-    const DynamoDBStore = dynamoDB(session)
-    store = new DynamoDBStore(dynamoOptions)
+    const RedisStore = redisStore(session)
+    const client = redis.createClient()
+    app.use(session({
+      store: new RedisStore({
+        client,
+        host: process.env.SESSION_HOST,
+        port: process.env.SESSION_PORT,
+        password: process.env.SESSION_PASSWORD,
+        username: process.env.SESSION_USER
+      }),
+      secret: process.env.COOKIE_SECRET,
+      key: 'user_sid',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge }
+    }))
   } else {
-    // TODO: Postgres as local session store not working
-    const postgresOptions = {
-      pool: pg.Pool({
-        user: process.env.RDS_USERNAME,
-        host: process.env.RDS_HOSTNAME,
-        database: process.env.RDS_DB_NAME,
-        password: process.env.RDS_PASSWORD,
-        port: process.env.RDS_PORT
-      })
-    }
-    const PostgresStore = postgres(session)
-    // store = new PostgresStore(postgresOptions)
     store = null
+    app.use(session({
+      store,
+      secret: process.env.COOKIE_SECRET,
+      key: 'user_sid',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge }
+    }))
   }
-
-  app.use(session({
-    store,
-    secret: process.env.COOKIE_SECRET,
-    key: 'user_sid',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge }
-  }))
 }
